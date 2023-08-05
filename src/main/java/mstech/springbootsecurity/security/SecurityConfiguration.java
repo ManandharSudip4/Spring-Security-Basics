677a67f07a2ae4873404ad5c91253198e1292638
ManandharSudip4 <manandharsudip8@gmail.com>
2023-08-05 13:51:34 +0545

    Minor Changes

--- src/main/java/mstech/springbootsecurity/security/SecurityConfiguration.java
+++ src/main/java/mstech/springbootsecurity/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
   private static final String myAdmin = "admin";
